2b231598bbaa49dd8d5435da055667b7dc36eaf8
Mattias Schertell <schertell@gmail.com>
2016-11-18 10:44:43 +0100

    Förslag
    på hur du skulle kunna göra istället

--- game/weapons.py
+++ game/weapons.py
@@ -1,3 +1,14 @@
+"""
+Dina funktioner innehåller onödig kod.
+Istället för att skapa listor och sedan göra slices av dessa, så skulle du kunna returnera strängarna direkt.
+Se nedan i exempel def sword().
+
+Ett annat alternativ är ju att skapa weaponlist globalt i scriptet (som då innehåller alla vapen).
+Sedan, i varje funktion kan man returnera specikfikt vapen på ett specifikt index.
+Se exempel sist i den här filen.
+"""
+
+
 def dagger():
     weaponlist = ["dagger"]
     givenweapon = weaponlist[:1]
@@ -14,3 +25,10 @@ def axe():
     weaponlist = ["axe"]
     givenweapon = weaponlist[:1]
     return givenweapon[0]
+
+def sword():
+    return "sword"
+
+weapons = ["shotgun", "sword", "cannon"]
+def getWeapon(index):
+    return weapons[index]
